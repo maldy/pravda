52ecd6c8eb1bffe983e92cf4ce50b5b0d568b441
Ratan Sebastian <rjsvaljean@gmail.com>
2011-01-19 20:39:34 +0530

    added db connection and user model

--- server.js
+++ server.js
@@ -1,13 +1,80 @@
-var express= require('express')
-var connect= require('connect')
+require('sys')
 require('ejs')
-var app = express.createServer();
+var express  = require('express')
+var app      = express.createServer();
+
+//start model section
+var mongoose = require('mongoose').Mongoose
+
+mongoose.model('User', {
+
+    properties: ['name', 'prefs'],
+
+    indexes: ['name'],
+
+    setters: {
+//      first: function(v){
+//          return this.v.capitalize();
+//      }
+    },
+
+    getters: {
+//      full_name: function(){ 
+//          return this.first + ' ' + this.last 
+//      }
+    },
+
+    methods: {
+        save: function(fn){
+            this.updated_at = new Date();
+            this.__super__(fn);
+        }
+    },
+});
+
+var db     = mongoose.connect('mongodb://localhost/test')
+var User   = db.model('User')
+// End model section
+
+// Start app section
 app.configure(function() {
     app.set('views', __dirname + '/views');
-    app.use(connect.staticProvider(__dirname + '/static'));
+    app.use(express.staticProvider(__dirname + '/static'));
+    app.use(express.bodyDecoder());
 })
 
 app.get('/', function(req, res){
+    res.render('home.ejs', {
+        locals: {
+            title: 'Home',
+            header: '',
+            footer: '',
+            description: '',
+            author: ''
+        }
+    }) 
+});
+
+
+app.post('/users', function(req, res){
+    var user_name= req.body.user.name
+    User.find({name: user_name}).all(function(result){
+        if(result.length > 0) {
+            console.log(user_name+' exists!')
+            console.log(result)
+        }
+        else {
+            console.log('User ' + user_name + ' not found')
+            var u = new User()
+            u.name = user_name
+            u.prefs = 'all'
+            u.save(function() {console.log(user_name+' Saved!')})
+        }
+    })
+    res.redirect('/'+user_name)
+});
+
+app.get('/:username', function(req, res) {
     res.render('index.ejs', { 
         locals: {
             header: 'Header',
